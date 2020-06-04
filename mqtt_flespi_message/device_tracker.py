b63ab16c524eec39917b5a9860189cd04b866477
Evgenij Spitsyn <evgeniy@spitsyn.net>
2020-06-04 18:11:38 +0300

    Style fixes

--- mqtt_flespi_message/device_tracker.py
+++ mqtt_flespi_message/device_tracker.py
@@ -9,33 +9,35 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components import mqtt
-from homeassistant.core import callback
-from homeassistant.components.mqtt import CONF_QOS
 from homeassistant.components.device_tracker import PLATFORM_SCHEMA
+from homeassistant.components.mqtt import CONF_QOS
+from homeassistant.const import (
+    CONF_DEVICES,
+)
+from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import ( CONF_DEVICES )
 
-ATTR_GPS_ACCURACY = 'position.hdop' # flespi accuracy parameter
-ATTR_LATITUDE = 'position.latitude'
-ATTR_LONGITUDE = 'position.longitude'
-ATTR_BATTERY_LEVEL = 'battery.level'
-
-
-
+ATTR_GPS_ACCURACY = "position.hdop" # flespi accuracy parameter
+ATTR_LATITUDE = "position.latitude"
+ATTR_LONGITUDE = "position.longitude"
+ATTR_BATTERY_LEVEL = "battery.level"
 
 
 _LOGGER = logging.getLogger(__name__)
 
-GPS_JSON_PAYLOAD_SCHEMA = vol.Schema({
-    vol.Required(ATTR_LATITUDE): vol.Coerce(float),
-    vol.Required(ATTR_LONGITUDE): vol.Coerce(float),
-    vol.Optional(ATTR_GPS_ACCURACY): vol.Coerce(int),
-    vol.Optional(ATTR_BATTERY_LEVEL): vol.Coerce(str),
-}, extra=vol.ALLOW_EXTRA)
+GPS_JSON_PAYLOAD_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_LATITUDE): vol.Coerce(float),
+        vol.Required(ATTR_LONGITUDE): vol.Coerce(float),
+        vol.Optional(ATTR_GPS_ACCURACY): vol.Coerce(int),
+        vol.Optional(ATTR_BATTERY_LEVEL): vol.Coerce(str),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend({
-    vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic},
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(mqtt.SCHEMA_BASE).extend(
+    {vol.Required(CONF_DEVICES): {cv.string: mqtt.valid_subscribe_topic}}
+)
 
 
 async def async_setup_scanner(hass, config, async_see, discovery_info=None):
@@ -43,6 +45,7 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
     qos = config[CONF_QOS]
 
     for dev_id, topic in devices.items():
+
         @callback
         def async_message_received(msg, dev_id=dev_id):
             """Handle received MQTT message."""
@@ -62,20 +65,16 @@ async def async_setup_scanner(hass, config, async_see, discovery_info=None):
             kwargs = _parse_see_args(dev_id, data)
             hass.async_create_task(async_see(**kwargs))
 
-        await mqtt.async_subscribe(
-            hass, topic, async_message_received, qos)
+        await mqtt.async_subscribe(hass, topic, async_message_received, qos)
 
     return True
 
 
 def _parse_see_args(dev_id, data):
-    kwargs = {
-        'gps': (data[ATTR_LATITUDE], data[ATTR_LONGITUDE]),
-        'dev_id': dev_id
-    }
+    kwargs = {"gps": (data[ATTR_LATITUDE], data[ATTR_LONGITUDE]), "dev_id": dev_id}
 
     if ATTR_GPS_ACCURACY in data:
-        kwargs['gps_accuracy'] = data[ATTR_GPS_ACCURACY]
+        kwargs["gps_accuracy"] = data[ATTR_GPS_ACCURACY]
     if ATTR_BATTERY_LEVEL in data:
-        kwargs['battery'] = data[ATTR_BATTERY_LEVEL]
+        kwargs["battery"] = data[ATTR_BATTERY_LEVEL]
     return kwargs
